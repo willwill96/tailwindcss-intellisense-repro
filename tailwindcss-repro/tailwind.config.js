785ede1cd5b271af05388175114a8822f37dc645
William Will <wbw4sv@virginia.edu>
2022-02-20 17:44:00 +0000

    add code

---
+++ tailwindcss-repro/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  prefix: '@',
+  content: [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    colors: {
+      newColor: {
+        100: '#ffffff',
+        200: '#fffffd',
+
+        DEFAULT: '#fffff9',
+        300: '#fffffc',
+
+        400: '#fffffb',
+
+        500: '#fffffa',
+
+      }
+    },
+    extend: {
+
+      }
+      
+  },
+  plugins: [],
+}
